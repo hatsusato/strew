b9e6b9b3b03381c78154aef3a5c80f6c59fce213
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-04-29 20:36:33 +0900

    Add char.sr to sample

--- test.sh
+++ test.sh
@@ -16,14 +16,56 @@ assert() {
     echo OK: "$PROGRAM" "$@"
   else
     cat <<EOF
-$PROGRAM ${args[@]@Q}
-EXPECTS: ${result[@]@Q}
-OUTPUTS: ${ret[@]@Q}
+NG: $PROGRAM ${args[@]@Q}
+    EXPECTS: ${result[@]@Q}
+    OUTPUTS: ${ret[@]@Q}
 EOF
     ERROR+=1
   fi
   return $err
 }
+
+PROGRAM=char.sr
+assert $'\a' EXPECTS bell
+assert $'\b' EXPECTS backspace
+assert $'\t' EXPECTS horizontal tab
+assert $'\n' EXPECTS newline
+assert $'\v' EXPECTS vertical tab
+assert $'\f' EXPECTS form feed
+assert $'\r' EXPECTS carriage ret
+assert ' ' EXPECTS space
+assert '!' EXPECTS exclamation mark
+assert '"' EXPECTS quotation mark
+assert '#' EXPECTS number sign
+assert '$' EXPECTS dollar sign
+assert '%' EXPECTS percent sign
+assert '&' EXPECTS ampersand
+assert "'" EXPECTS apostrophe
+assert '(' EXPECTS left parenthesis
+assert ')' EXPECTS right parenthesis
+assert '*' EXPECTS asterisk
+assert '+' EXPECTS plus sign
+assert ',' EXPECTS comma
+assert '-' EXPECTS hyphen minus
+assert '.' EXPECTS full stop
+assert '/' EXPECTS slash
+assert ':' EXPECTS colon
+assert ';' EXPECTS semicolon
+assert '<' EXPECTS less-than sign
+assert '=' EXPECTS equals sign
+assert '>' EXPECTS greater-than sign
+assert '?' EXPECTS question mark
+assert '@' EXPECTS at sign
+assert '[' EXPECTS left square bracket
+assert '\' EXPECTS backslash
+assert ']' EXPECTS right square bracket
+assert '^' EXPECTS caret
+assert '_' EXPECTS underscore
+assert '`' EXPECTS grave accent
+assert '{' EXPECTS left curly bracket
+assert '|' EXPECTS vertical bar
+assert '}' EXPECTS right curly bracket
+assert '~' EXPECTS tilde
 
 PROGRAM=tm-0n1n.sr
 for i in 0 00 000 0000; do
